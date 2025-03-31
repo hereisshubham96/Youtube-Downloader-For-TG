f83959ddb05c71c49694c0c4727fcf059839c835
hereisshubham96 <shubham_@outlook.in>
2025-03-31 16:02:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import  os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-APP_ID = int(os.environ.get("API_ID"))
-API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7233948862:AAGYF0sz8g1JKFN6IgDRJOPeoaeKQxtoonw")
+APP_ID = int(os.environ.get("API_ID", "28713615"))
+API_HASH = os.environ.get("API_HASH", "bc22ae2558b1de94fee467d91f1e337f")
 G1N = os.environ.get("GROUP_1_NAME")
 G2N = os.environ.get("GROUP_2_NAME")
 G1L = os.environ.get("GROUP_1_LINK")
